6ee273e2cdaf9a68e27681dec6077ac987f9e9a2
Francesco Rizzi <fnrizzi@sandia.gov>
2022-01-18 17:27:38 +0100

    1d: add Shu-Osher problem

--- include/pressiodemoapps/impl/euler_1d_initial_condition.hpp
+++ include/pressiodemoapps/impl/euler_1d_initial_condition.hpp
@@ -92,5 +92,44 @@ void lax1dInitialCondition(state_type & state,
     }
 }
 
+template<class state_type, class mesh_t, class scalar_type>
+void shuOsherInitialCondition(state_type & state,
+			   const mesh_t & meshObj,
+			   const scalar_type gamma)
+{
+
+  constexpr int numDofPerCell = 3;
+  constexpr auto zero    = static_cast<scalar_type>(0);
+  constexpr auto one     = static_cast<scalar_type>(1);
+  constexpr auto three   = static_cast<scalar_type>(3);
+  constexpr auto four    = static_cast<scalar_type>(4);
+  constexpr auto negFour = -four;
+  constexpr auto five    = static_cast<scalar_type>(5);
+  constexpr auto seven   = static_cast<scalar_type>(7);
+  constexpr auto twentySeven  = static_cast<scalar_type>(27);
+  constexpr auto thirtyOne  = static_cast<scalar_type>(31);
+
+  const auto & x= meshObj.viewX();
+  std::array<scalar_type, numDofPerCell> prim;
+  for (int i=0; i<::pressiodemoapps::extent(x,0); ++i)
+    {
+      const auto ind = i*numDofPerCell;
+      if (x(i) <= negFour){
+	prim[0] = twentySeven/seven;
+	prim[1] = static_cast<scalar_type>(2.629369);
+	prim[2] = thirtyOne/three;
+      }
+      else{
+	prim[0] = one + (one/five)*std::sin(five*x(i));
+	prim[1] = zero;
+	prim[2] = one;
+      }
+
+      state(ind)   = prim[0];
+      state(ind+1) = prim[0]*prim[1];
+      state(ind+2) = eulerEquationsComputeEnergyFromPrimitive(gamma, prim);
+    }
+}
+
 }}//end namespace
 #endif
