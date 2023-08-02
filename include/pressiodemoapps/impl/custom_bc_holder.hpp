9646380d28e28024c71782f728dd20941e850611
Francesco Rizzi <fnrizzi@sandia.gov>
2023-08-02 18:03:12 +0200

    rename

--- include/pressiodemoapps/impl/custom_bc_holder.hpp
+++ include/pressiodemoapps/impl/custom_bc_holder.hpp
@@ -54,73 +54,76 @@
 namespace pressiodemoapps{
 namespace impl{
 
-template<class T1, class T2, class T3, class T4>
+template<class FuncLeftT, class FuncFrontT, class FuncRightT, class FuncBackT>
 struct CustomBCsHolder;
 
-template<class T1, class T2, class T3, class T4>
+template<class FuncLeftT, class FuncFrontT, class FuncRightT, class FuncBackT>
 struct CustomBCsHolder
 {
   CustomBCsHolder() = delete;
 
-  template<class T1l, class T2l, class T3l, class T4l>
-  CustomBCsHolder(T1l && t1l, T2l && t2l, T3l && t3l, T4l && t4l)
-    : a_(std::forward<T1l>(t1l)),
-      b_(std::forward<T2l>(t2l)),
-      c_(std::forward<T3l>(t3l)),
-      d_(std::forward<T4l>(t4l)){}
+  template<class T1, class T2, class T3, class T4>
+  CustomBCsHolder(T1 && a, T2 && b, T3 && c, T4 && d)
+    : m_funcLeft(std::forward<T1>(a)),
+      m_funcFront(std::forward<T2>(b)),
+      m_funcRight(std::forward<T3>(c)),
+      m_funcBack(std::forward<T4>(d)){}
 
   template<class ...Types>
   void operator() (GhostRelativeLocation rloc, Types && ... args) const noexcept
   {
     if (rloc == GhostRelativeLocation::Left){
-      a_(std::forward<Types>(args)...);
+      m_funcLeft(std::forward<Types>(args)...);
     }
     else if (rloc == GhostRelativeLocation::Front){
-      b_(std::forward<Types>(args)...);
+      m_funcFront(std::forward<Types>(args)...);
     }
     else if (rloc == GhostRelativeLocation::Right){
-      c_(std::forward<Types>(args)...);
+      m_funcRight(std::forward<Types>(args)...);
     }
     else{
-      d_(std::forward<Types>(args)...);
+      m_funcBack(std::forward<Types>(args)...);
     }
   }
 
-  T1 a_;
-  T2 b_;
-  T3 c_;
-  T4 d_;
+private:
+  FuncLeftT m_funcLeft;
+  FuncFrontT m_funcFront;
+  FuncRightT m_funcRight;
+  FuncBackT m_funcBack;
 };
 
-template<class T1, class T2, class T3, class T4>
-struct CustomBCsHolder<T1 &, T2 &, T3 &, T4 &>
+template<class FuncLeftT, class FuncFrontT, class FuncRightT, class FuncBackT>
+struct CustomBCsHolder<FuncLeftT &, FuncFrontT &, FuncRightT &, FuncBackT &>
 {
   CustomBCsHolder() = delete;
 
-  CustomBCsHolder(T1 & t1l, T2 & t2l, T3 & t3l, T4 & t4l)
-    : a_(t1l), b_(t2l), c_(t3l), d_(t4l){}
+  CustomBCsHolder(FuncLeftT & a, FuncFrontT & b,
+		  FuncRightT & c, FuncBackT & d)
+    : m_funcLeft(a), m_funcFront(b), m_funcRight(c), m_funcBack(d){}
 
   template<class ...Types>
   void operator() (GhostRelativeLocation rloc, Types && ... args) const noexcept
   {
     if (rloc == GhostRelativeLocation::Left){
-      a_(std::forward<Types>(args)...);
+      m_funcLeft(std::forward<Types>(args)...);
     }
     else if (rloc == GhostRelativeLocation::Front){
-      b_(std::forward<Types>(args)...);
+      m_funcFront(std::forward<Types>(args)...);
     }
     else if (rloc == GhostRelativeLocation::Right){
-      c_(std::forward<Types>(args)...);
+      m_funcRight(std::forward<Types>(args)...);
     }
     else{
-      d_(std::forward<Types>(args)...);
+      m_funcBack(std::forward<Types>(args)...);
     }
   }
 
-  std::reference_wrapper<const T1> a_;
-  std::reference_wrapper<const T2> b_;
-  std::reference_wrapper<const T3> c_;
-  std::reference_wrapper<const T4> d_;
+private:
+  std::reference_wrapper<const FuncLeftT> m_funcLeft;
+  std::reference_wrapper<const FuncFrontT> m_funcFront;
+  std::reference_wrapper<const FuncRightT> m_funcRight;
+  std::reference_wrapper<const FuncBackT> m_funcBack;
 };
 
 }} //end namespace pressiodemoapps::impl
